f982a8964e3677dd91f3b0232d5653c2e017b233
joandlee <60385129+joandlee@users.noreply.github.com>
2020-11-15 11:50:17 +1100

    Update models.py

--- thumbnail/models.py
+++ thumbnail/models.py
@@ -9,14 +9,15 @@ class AsyncThumbnailMixin(object):
     """
     All model which have ImageField to be thumbnailed inheret from this class.
     """
-    image_field_name = ['picture']
+    image_field_name = [{'field': 'picture', 'is_cover': False}]
 
     def call_upload_task(self):
         for name, options in settings.OPTIONS_DICT.items():
             opt = copy(options)
             geometry = opt.pop('geometry')
             for img in self.image_field_name:
-                create_thumbnail.delay(getattr(self, img), geometry, **opt)
+                if (name == 'cover' and img["is_cover"]) or (name != 'cover'):
+                    create_thumbnail.delay(getattr(self, img["field"]), geometry, **opt)
 
     def save(self, *args, **kwargs):
         super(AsyncThumbnailMixin, self).save(*args, **kwargs)
